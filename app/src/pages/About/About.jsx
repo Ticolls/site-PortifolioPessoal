069a304a30e42d089b3e0d5009551d5ceed4d250
Thiago Seixas <ticolls@hotmail.com>
2023-03-06 17:14:21 -0300

    alinhando text do contact

--- app/src/pages/About/About.jsx
+++ app/src/pages/About/About.jsx
@@ -17,7 +17,7 @@ export function About() {
                     <header><BulletPoint />About</header>
                     <h3>Conheça um pouco sobre mim</h3>
                     <p>Olá, me chamo Thiago Luiz Antunes Seixas, tenho 19 anos e sou estudante de Ciências da Computação pela Universidade Federal da Bahia (UFBA). Estudo programação desde os meus 16 anos durante meu período colegial. De cara me apaixonei pela área e vi a oportunidade de transformar um hobby em trabalho.</p>
-                    <p>Com isso, venho estudando tanto pela faculdade quanto de forma autônoma para aperfeiçoar minhas habilidades e competências na área de desenvolvimento que eu tanto amo. Atualmente, estudo e trabalho com desenvolvimento web FullStack.</p>
+                    <p>Com isso, venho estudando tanto pela faculdade quanto de forma autônoma para aperfeiçoar minhas habilidades e competências na área de desenvolvimento que eu tanto amo. Atualmente estudo com desenvolvimento web FullStack.</p>
                 </div>
             </div>
         </section>
